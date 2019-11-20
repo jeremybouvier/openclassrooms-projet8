ca8f36c1d90a4491f555b69686cc869333638caa
jeremy bouvier <jeremybouvier2b@gmail.com>
2019-11-20 23:22:01 +0100

    add handler unit tests

--- tests/AppBundle/Handler/TaskHandlerTest.php
+++ tests/AppBundle/Handler/TaskHandlerTest.php
@@ -1,48 +1,91 @@
 <?php
 
-
 namespace Tests\AppBundle\Handler;
 
-
-
 use AppBundle\Entity\Task;
+use AppBundle\Entity\User;
 use AppBundle\Handler\TaskHandler;
 use Doctrine\ORM\EntityManagerInterface;
+use Doctrine\ORM\UnitOfWork;
 use PHPUnit\Framework\TestCase;
 use Symfony\Component\Form\FormFactoryInterface;
 use Symfony\Component\Form\FormInterface;
 use Symfony\Component\HttpFoundation\Request;
 use Symfony\Component\HttpFoundation\Session\Flash\FlashBagInterface;
+use Symfony\Component\Security\Core\Authentication\Token\AbstractToken;
 use Symfony\Component\Security\Core\Authentication\Token\Storage\TokenStorageInterface;
-
 
 class TaskHandlerTest extends TestCase
 {
     /**
-     * Test du handle d'un tache
+     * @var TaskHandler
      */
-    public function testTaskHandle()
+    private $taskHandler;
+
+    /**
+     * TaskHandlerTest constructor.
+     * @param null $name
+     * @param array $data
+     * @param string $dataName
+     */
+    public function __construct($name = null, array $data = [], $dataName = '')
     {
+        parent::__construct($name, $data, $dataName);
+        $unitOfWork = $this->createMock(UnitOfWork::class);
+        $unitOfWork->method("getEntityState")->willReturn(UnitOfWork::STATE_NEW);
         $entityManager = $this->createMock(EntityManagerInterface::class);
-        //$entityManager->method('getEntityState')->willReturn('STATE_NEW');
+        $entityManager->method('getUnitOfWork')->willReturn($unitOfWork);
         $flashbag = $this->createMock(FlashBagInterface::class);
         $tokenStorage = $this->createMock(TokenStorageInterface::class);
-        $taskHandler = new TaskHandler($entityManager, $flashbag, $tokenStorage);
+        $token = $this->createMock(AbstractToken::class);
+        $token->method('getUser')->willReturn(new User);
+        $tokenStorage->method('getToken')->willReturn($token);
+        $this->taskHandler = new TaskHandler($entityManager, $flashbag, $tokenStorage);
+    }
 
-        $form = $this->createMock(FormInterface::class);
-        $form->method("isSubmitted")->willReturn(true);
-        $form->method("isValid")->willReturn(true);
-        $form->method("handleRequest")->willReturnSelf();
+    /**
+     * Test de la méthode handle
+     */
+    public function testHandle()
+    {
+        $this->assertTrue($this->taskHandler->handle($this->submitForm(true), new Task()));
+        $this->assertFalse($this->taskHandler->handle($this->submitForm(false), new Task()));
+    }
 
-
-        $formFactory = $this->createMock(FormFactoryInterface::class);
-        $formFactory->method('create')->willReturn($form);
-        $taskHandler->setFormFactory($formFactory);
-        $request = Request::create("/");
-
-        $this->assertTrue($taskHandler->handle($request, new Task()));
-
+    /**
+     * Test de la méthode CreateView
+     */
+    public function testCreateView()
+    {
+        $this->taskHandler->handle($this->submitForm(true), new Task());
+        $this->assertNull($this->taskHandler->createView());
 
     }
 
+    /**
+     * Test de méthode GetData
+     */
+    public function testGetData()
+    {
+        $this->taskHandler->handle($this->submitForm(true), new Task());
+        $this->assertInstanceOf(Task::class, $this->taskHandler->getData());
+    }
+
+    /**
+     * Création et soumission du formulaire
+     * @param $validation
+     * @return Request
+     */
+    private function submitForm($validation)
+    {
+        $form = $this->createMock(FormInterface::class);
+        $form->method("isSubmitted")->willReturn($validation);
+        $form->method("isValid")->willReturn($validation);
+        $form->method("handleRequest")->willReturnSelf();
+        $formFactory = $this->createMock(FormFactoryInterface::class);
+        $formFactory->method('create')->willReturn($form);
+        $this->taskHandler->setFormFactory($formFactory);
+        $request = Request::create("/");
+        return $request;
+    }
 }
--- tests/AppBundle/Handler/UserHandlerTest.php
+++ tests/AppBundle/Handler/UserHandlerTest.php
@@ -1,11 +1,11 @@
 <?php
 
-
 namespace Tests\AppBundle\Handler;
 
 use AppBundle\Entity\User;
 use AppBundle\Handler\UserHandler;
 use Doctrine\ORM\EntityManagerInterface;
+use Doctrine\ORM\UnitOfWork;
 use PHPUnit\Framework\TestCase;
 use Symfony\Component\Form\FormFactoryInterface;
 use Symfony\Component\Form\FormInterface;
@@ -16,30 +16,71 @@ use Symfony\Component\Security\Core\Encoder\UserPasswordEncoderInterface;
 class UserHandlerTest extends TestCase
 {
     /**
-     * Test du handle d'un user
+     * @var UserHandler
      */
-    public function testUserHandle()
+    private $userHandler;
+
+    /**
+     * UserHandlerTest constructor.
+     * @param null $name
+     * @param array $data
+     * @param string $dataName
+     */
+    public function __construct($name = null, array $data = [], $dataName = '')
     {
+        parent::__construct($name, $data, $dataName);
+        $unitOfWork = $this->createMock(UnitOfWork::class);
+        $unitOfWork->method("getEntityState")->willReturn(UnitOfWork::STATE_NEW);
         $entityManager = $this->createMock(EntityManagerInterface::class);
-        //$entityManager->method('getEntityState')->willReturn('STATE_NEW');
+        $entityManager->method('getUnitOfWork')->willReturn($unitOfWork);
         $flashbag = $this->createMock(FlashBagInterface::class);
         $encoder = $this->createMock(UserPasswordEncoderInterface::class);
-        $userHandler = new UserHandler($entityManager, $flashbag, $encoder);
+        $this->userHandler = new UserHandler($entityManager, $flashbag, $encoder);
+    }
 
-        $form = $this->createMock(FormInterface::class);
-        $form->method("isSubmitted")->willReturn(true);
-        $form->method("isValid")->willReturn(true);
-        $form->method("handleRequest")->willReturnSelf();
+    /**
+     * Test de la méthode handle
+     */
+    public function testHandle()
+    {
+        $this->assertTrue($this->userHandler->handle($this->submitForm(true), new User));
+        $this->assertFalse($this->userHandler->handle($this->submitForm(false), new User));
+    }
 
-
-        $formFactory = $this->createMock(FormFactoryInterface::class);
-        $formFactory->method('create')->willReturn($form);
-        $userHandler->setFormFactory($formFactory);
-        $request = Request::create("/");
-
-        $this->assertTrue($userHandler->handle($request, new User()));
-
+    /**
+     * Test de la méthode CreateView
+     */
+    public function testCreateView()
+    {
+        $this->userHandler->handle($this->submitForm(true), new User);
+        $this->assertNull($this->userHandler->createView());
 
     }
 
+    /**
+     * Test de méthode GetData
+     */
+    public function testGetData()
+    {
+        $this->userHandler->handle($this->submitForm(true), new User);
+        $this->assertInstanceOf(User::class, $this->userHandler->getData());
+    }
+
+    /**
+     * Création et soumission du formulaire
+     * @param $validation
+     * @return Request
+     */
+    private function submitForm($validation)
+    {
+        $form = $this->createMock(FormInterface::class);
+        $form->method("isSubmitted")->willReturn($validation);
+        $form->method("isValid")->willReturn($validation);
+        $form->method("handleRequest")->willReturnSelf();
+        $formFactory = $this->createMock(FormFactoryInterface::class);
+        $formFactory->method('create')->willReturn($form);
+        $this->userHandler->setFormFactory($formFactory);
+        $request = Request::create("/");
+        return $request;
+    }
 }
