ce26c18b7628d49ead76e458691dfd6e04875dd8
jeremybouvier <47010373+jeremybouvier@users.noreply.github.com>
2019-11-21 22:04:47 +0100

    add Voter unit tests

--- app/AppKernel.php
+++ app/AppKernel.php
@@ -26,6 +26,9 @@ class AppKernel extends Kernel
             $bundles[] = new Sensio\Bundle\DistributionBundle\SensioDistributionBundle();
             $bundles[] = new Sensio\Bundle\GeneratorBundle\SensioGeneratorBundle();
             $bundles[] = new Doctrine\Bundle\FixturesBundle\DoctrineFixturesBundle();
+            if ($this->getEnvironment() === 'test') {
+                $bundles[] = new DAMA\DoctrineTestBundle\DAMADoctrineTestBundle();
+            }
         }
 
         return $bundles;
